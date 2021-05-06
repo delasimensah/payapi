1adb57eb43bb162498207ed2addbaa7a888f84e6
delasimensah <mensadelasi@gmail.com>
2021-05-06 16:30:25 +0000

    started pricing page

---
+++ components/Price.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Price = ({ info }) => {
+  return (
+    <div>
+      <h1>{info.plan}</h1>
+    </div>
+  );
+};
+
+export default Price;
--- pages/pricing.jsx
+++ pages/pricing.jsx
@@ -1,9 +1,131 @@
 import React from "react";
 
 //components
+import Heading from "../components/Heading";
+import Price from "../components/Price";
+import Circle from "../components/Circle";
 
 const Pricing = () => {
-  return <div></div>;
+  const pricing = [
+    {
+      plan: "Free Plan",
+      desc: "Build and test using our core set of products with up to 100 API requests",
+      price: "0.00",
+      features: [
+        {
+          title: "Transactions",
+          available: true,
+        },
+        {
+          title: "Auth",
+          available: true,
+        },
+        {
+          title: "Identity",
+          available: true,
+        },
+        {
+          title: "Investments",
+          available: false,
+        },
+        {
+          title: "Assets",
+          available: false,
+        },
+        {
+          title: "Liabilities",
+          available: false,
+        },
+        {
+          title: "Income",
+          available: false,
+        },
+      ],
+    },
+    {
+      plan: "Basic Plan",
+      desc: "Launch your project with unlimited requests and no contractual minimums",
+      price: "249.00",
+      features: [
+        {
+          title: "Transactions",
+          available: true,
+        },
+        {
+          title: "Auth",
+          available: true,
+        },
+        {
+          title: "Identity",
+          available: true,
+        },
+        {
+          title: "Investments",
+          available: true,
+        },
+        {
+          title: "Assets",
+          available: true,
+        },
+        {
+          title: "Liabilities",
+          available: false,
+        },
+        {
+          title: "Income",
+          available: false,
+        },
+      ],
+    },
+    {
+      plan: "Premium Plan",
+      desc: "Get tailored solutions, volume pricing, and dedicated support for your team",
+      price: "499.00",
+      features: [
+        {
+          title: "Transactions",
+          available: true,
+        },
+        {
+          title: "Auth",
+          available: true,
+        },
+        {
+          title: "Identity",
+          available: true,
+        },
+        {
+          title: "Investments",
+          available: true,
+        },
+        {
+          title: "Assets",
+          available: true,
+        },
+        {
+          title: "Liabilities",
+          available: true,
+        },
+        {
+          title: "Income",
+          available: true,
+        },
+      ],
+    },
+  ];
+  return (
+    <>
+      <Circle />
+
+      <Heading>Pricing</Heading>
+
+      <div className="grid gap-10 wrapper md:grid-cols-3">
+        {pricing.map((info, idx) => {
+          return <Price info={info} key={idx} />;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Pricing;
